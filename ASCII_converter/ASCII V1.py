079498a86abae8df08bce1c6e12670294d8c3991
AntoninMayet <antoninmayet63@gmail.com>
2023-06-20 18:11:46 +0200

    I don't understand why the code return a permission error when I ask to create a file. It does it even when I open VSCode in admin mode  Changes to be committed: 	modified:   ASCII_converter/ASCII V1.py

--- ASCII_converter/ASCII V1.py
+++ ASCII_converter/ASCII V1.py
@@ -1,40 +1,26 @@
-#DONE Get the image
-#TODO Get the dimension of the image
-#TODO Convert it in B&W
-#TODO Get the value of each pixel
-#TODO Convert the value in a char
-#TODO Print each char on a grid the same size of the image
-#TODO Open the coolest version of the image
+from PIL import Image
 
-from PIL import Image as im
+density = ' .:-=+*#%@'
 
-density =  ' .:-=+*#%@'
+file_path = input('Give the path to your desired image : ')
+text_file_path = input('Give the path to save the text file : ')
 
-file_path = (input('Give the path to your desire image :\n'))
+image = Image.open(file_path)
+bw_image = image.convert('L')
 
-u_file = im.open(file_path)
+file_width, file_height = image.size
 
-bw_image = u_file.convert('L')
-
-file_width, file_height = u_file.size
-
-'''
-print('w', file_width)
-print('h', file_height)
-'''
-
-for x in range(file_width):
+with open(text_file_path, 'w') as text_file:
     for y in range(file_height):
-       
-        pixel_lum = bw_image.getpixel((x, y))
-        lum = pixel_lum / 255
-       
-        index = int(lum * (len(density) - 1))
-        ASCII = density[index]
-        print(ASCII, end=' ')
-       
-        count =+ 1
-       
-        if count > file_height:
-            print('\n')
-            cout = 0
+        for x in range(file_width):
+            pixel_lum = bw_image.getpixel((x, y))
+            lum = pixel_lum / 255
+
+            index = int(lum * (len(density) - 1))
+            ASCII = density[index]
+
+            print(ASCII, end=' ')
+            text_file.write(ASCII)
+
+        print()
+        text_file.write('\n')
