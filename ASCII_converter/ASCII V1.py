0db55c37ade15c6c57f5ec254c109a0ae01e28de
AntoninMayet <antoninmayet63@gmail.com>
2023-06-23 09:43:37 +0200

    ¬_¬ The programm is able to create a new image but it doesn't work  Changes to be committed: 	modified:   ASCII_converter/ASCII V1.py 	deleted:    ASCII_converter/Jadoune40.txt

--- ASCII_converter/ASCII V1.py
+++ ASCII_converter/ASCII V1.py
@@ -1,9 +1,17 @@
 from PIL import Image, ImageFont, ImageDraw
 import os
+import time
+
+file_path = input('Give the path to your desired image : ')
+
+start_time = time.time()
 
 density = ' .:-=+*#%@'
 
-file_path = input('Give the path to your desired image : ')
+font_name = r'C:\Users\anton\.vscode\Coding_projects\ASCII_converter\CourierPrime-Regular.ttf'
+font_size = 12
+spacing = 1
+font = ImageFont.truetype(font_name, font_size)
 
 text_file_path, image_name = os.path.split(file_path)
 image_name_without_extension = os.path.splitext(image_name)[0]
@@ -13,23 +21,29 @@ text_file_path = os.path.join(text_file_path, f'{image_name_without_extension}.t
 image = Image.open(file_path)
 bw_image = image.convert('L')
 
-file_width, file_height = image.size
+image_width, image_height = image.size
 
-font_name = r'C:\Users\anton\.vscode\Coding_projects\ASCII_converter\CourierPrime-Regular.ttf'
-font_size = 12
-spacing = 1
-font = ImageFont.truetype(font_name, font_size)
+result_image = Image.new('L', (image_width, image_height))
+draw = ImageDraw.Draw(result_image)
+draw.fontmode = '1'
 
 with open(text_file_path, 'w') as text_file:
-    for y in range(file_height):
-        for x in range(file_width):
+    for y in range(image_height):
+        for x in range(image_width):
             pixel_lum = bw_image.getpixel((x, y))
             lum = pixel_lum / 255
 
             index = int(lum * (len(density) - 1))
             ASCII = density[index]
 
-            print(ASCII, end=' ')
             text_file.write(ASCII)
 
+            draw.text((x * (font_size + spacing), y * font_size), ASCII, fill='white', font=font)
+
         text_file.write('\n')
+
+result_image.save('output_image.jpg')
+
+end_time = time.time()
+time_spend = end_time - start_time
+print('Time spend (ms) : ', time_spend)
